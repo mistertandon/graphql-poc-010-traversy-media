584a878c2a0b2102b88bb096e54973c257e0a8bc
mistertandon <enggparveshtandon@gmail.com>
2023-04-29 10:00:59 +0530

    feat: import GraphQL in express app. define statis schema configure GraphQLSchema using defined static schema Test basic GraphQL query

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 npm install express express-graphql graphql mongoose cors colors --save
 npm install  nodemon dotenv --save-dev
+
+```javascript
+{
+  client(id: "1") {
+    id
+    name
+    email
+    phone
+  }
+}
+```
--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
 require("dotenv").config();
 const express = require("express");
+const schema = require("./schema/schema.js");
 const port = process.env.PORT || 5000;
 const { graphqlHTTP } = require("express-graphql");
 
 const app = express();
-// app.use('/graphql', graphqlHTTP({}))
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === "development"?true:false
+}))
 
 app.listen(port, () => console.log(`server started on port ${port}`));
