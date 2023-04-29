e550241868447195f2a52770da8ec1e0cc284365
mistertandon <enggparveshtandon@gmail.com>
2023-04-30 04:27:15 +0530

    feat: Mongoose DB config file to get connected with mgmt_db

--- server/index.js
+++ server/index.js
@@ -1,13 +1,17 @@
 require("dotenv").config();
+const colors = require("colors");
 const express = require("express");
 const schema = require("./schema/schema.js");
-const port = process.env.PORT || 5000;
 const { graphqlHTTP } = require("express-graphql");
+
+const { connectDB } = require("./config/db.js");
+connectDB();
+const port = process.env.PORT || 5000;
 
 const app = express();
 app.use('/graphql', graphqlHTTP({
     schema,
-    graphiql: process.env.NODE_ENV === "development"?true:false
+    graphiql: process.env.NODE_ENV === "development" ? true : false
 }))
 
 app.listen(port, () => console.log(`server started on port ${port}`));
